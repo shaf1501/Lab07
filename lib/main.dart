b04420946001d3cfeadc4acc96364271a0e4b322
shaf1501 <shafin15-4903@diu.edu.bd>
2025-03-11 10:15:21 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class FreebeesEcom extends StatelessWidget {
+  const FreebeesEcom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Freebees E-com",
+      home: Home(),
+    );
+  }
+}
+// tinyurl.com/cse414-figma
+// Splash Screen
