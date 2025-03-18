0571c6826cef07ccd2082e3f56b38871588ba24f
shaf1501 <shafin15-4903@diu.edu.bd>
2025-03-18 10:04:05 +0600

    seperating_bottom_nav_bar

--- lib/screens/home/page/home.dart
+++ lib/screens/home/page/home.dart
@@ -2,10 +2,41 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/common/widget/appbar.dart';
+import 'package:flutter_application_1/screens/home/widgets/bottom_nav_bar.dart';
 import 'package:icons_plus/icons_plus.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _screens = [
+    Container(
+      height: double.infinity,
+      color: Colors.amber,
+    ),
+    Container(
+      height: double.infinity,
+      color: Colors.blue,
+    ),
+    Container(
+      height: double.infinity,
+      color: Colors.green,
+    ),
+    Container(
+      height: double.infinity,
+      color: Colors.red,
+    ),
+  ];
+  void _onTapMethod(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +45,11 @@ class Home extends StatelessWidget {
         title: "Mega Shop",
         firstIcon: Iconsax.notification_bing_outline,
         backButton: true,
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: FBottomBar(
+        currentIndex: _currentIndex,
+        onTapMethod: _onTapMethod,
       ),
     );
   }
---
+++ lib/screens/home/widgets/bottom_nav_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:icons_plus/icons_plus.dart';
+
+class FBottomBar extends StatelessWidget {
+  const FBottomBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTapMethod,
+  });
+
+  final int currentIndex;
+  final Function(int) onTapMethod;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTapMethod,
+      type: BottomNavigationBarType.fixed,
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Iconsax.home_outline),
+          label: "Home",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Iconsax.heart_outline),
+          label: "WISHLIST",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Iconsax.shopping_bag_outline),
+          label: "ORDER",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Iconsax.user_outline),
+          label: "ACCOUNT",
+        ),
+      ],
+    );
+  }
+}
