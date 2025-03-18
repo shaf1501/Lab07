8760717eccd9b4866b4ccb9aeab8a45c2b3c5fd2
shaf1501 <shafin15-4903@diu.edu.bd>
2025-03-18 10:37:22 +0600

    adding_image

--- lib/screens/home/page/home_screen.dart
+++ lib/screens/home/page/home_screen.dart
@@ -5,6 +5,64 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: Center(
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          elevation: 4,
+          child: Container(
+            width: 200,
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Image.network(
+                    'https://www.rawpixel.com/image/2767451/premium-illustration-psd-headphones-headphone-mockup-music#eyJrZXlzIjoiaGVhZHBob25lcyIsInNvcnRlZEtleXMiOiJoZWFkcGhvbmUifQ==', // Replace with actual image URL
+                    height: 100,
+                    errorBuilder: (context, error, stackTrace) {
+                      return const Icon(
+                        Icons.image_not_supported,
+                        size: 100,
+                        color: Colors.grey,
+                      );
+                    },
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'TMA-2 HD Wireless',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 5),
+                const Text(
+                  'Rp. 1.500.000',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: const [
+                    Icon(Icons.star, color: Colors.amber, size: 16),
+                    SizedBox(width: 5),
+                    Text(
+                      '4.6  •  86 Reviews',
+                      style: TextStyle(fontSize: 14, color: Colors.black54),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
